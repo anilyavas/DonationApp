d6ade3fff21cdd88ede7dbb9712dc4a5f9f0c4b9
Anıl <anil@Anl-MacBook-Air.local>
2023-08-24 17:29:47 +0300

    updated first time

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -4,6 +4,7 @@ import {View, SafeAreaView, Text} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
 import Tab from '../../components/Tab/Tab';
+import Badge from '../../components/Badge/Badge';
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
         <Header title={'Azzahri A.'} type={1} />
         <Tab title={'Highlight'} />
         <Tab title={'Highlight'} isInactive={true} />
+        <Badge title={'Environment'} />
       </View>
     </SafeAreaView>
   );
