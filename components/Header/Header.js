3d63d226ac401fc2e32970fa465b7edd81bdc58b
Anıl <anil@Anl-MacBook-Air.local>
2023-08-27 12:18:29 +0300

    updated 27th of August

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -18,16 +18,20 @@ const Header = props => {
   };
   return (
     <View>
-      <Text style={styleToApply}>{props.title}</Text>
+      <Text style={[styleToApply(), props.color && {color: props.color}]}>
+        {props.title}
+      </Text>
     </View>
   );
 };
 Header.default = {
   title: '',
   type: 1,
+  color: '#000000',
 };
 Header.propTypes = {
   title: PropTypes.string,
   types: PropTypes.number,
+  color: PropTypes.string,
 };
 export default Header;
