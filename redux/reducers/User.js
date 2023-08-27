6a3ada7aaa9da48d605416ea114d084a90b0e929
Anıl <anil@Anl-MacBook-Air.local>
2023-08-27 13:52:50 +0300

    Updated 27th of August 2nd time

---
+++ redux/reducers/User.js
@@ -0,0 +1,19 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+  firstName: 'Anil',
+  lastName: 'Yavas',
+  userId: 1,
+};
+export const User = createSlice({
+  name: 'user',
+  initialState: initialState,
+  reducers: {
+    updateFirstName: (state, action) => {
+      state.firstName = action.payload.firstName;
+    },
+  },
+});
+
+export const {updateFirstName} = User.actions;
+export default User.reducer;
