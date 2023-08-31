33e60904903d222687650ff5fe6d275149bb7696
Anıl <anil@Anl-MacBook-Air.local>
2023-08-31 13:18:46 +0300

    31st of August updated

--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -1,23 +1,45 @@
 import React, {useState} from 'react';
-import {SafeAreaView, ScrollView} from 'react-native';
+import {Pressable, SafeAreaView, ScrollView, View} from 'react-native';
 import Input from '../../components/Input/Input';
+import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyle';
 
 const Login = () => {
-  const [email, setEmail] = useState();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <SafeAreaView style={[globalStyle.flex, globalStyle.backgroundWhite]}>
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={style.container}>
-        <Input
-          keyboardType={'email-address'}
-          label={'Email'}
-          placeholder={'Enter your email...'}
-          onChangeText={value => setEmail(value)}
-        />
+        <View style={globalStyle.marginBottom24}>
+          <Header type={1} title={'Welcome Back'} />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            keyboardType={'email-address'}
+            label={'Email'}
+            placeholder={'Enter your email...'}
+            onChangeText={value => setEmail(value)}
+          />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            secureTextEntry={true}
+            label={'Password'}
+            placeholder={'******'}
+            onChangeText={value => setPassword(value)}
+          />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Button title={'Login'} />
+        </View>
+        <Pressable style={style.registrationButton}>
+          <Header color={'#156CF7'} type={3} title={"Don't have an account?"} />
+        </Pressable>
       </ScrollView>
     </SafeAreaView>
   );
